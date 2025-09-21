cbe1088f2077a7448155029a3ce5d16f86311fd4
Zanelisiwe <ntombelazanelisiwe@outlook.com>
2025-09-21 21:52:07 +0200

    add_API_ENDPOINT

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import health
+from .views import health, BookList
 
 urlpatterns = [
     path("health/", health, name="health"),
+    path("books/", BookList.as_view(), name="book-list"),
 ]
