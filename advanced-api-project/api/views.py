3179952472a8d58bef48ad626fc2925ad4fd71e3
Zanelisiwe <ntombelazanelisiwe@outlook.com>
2025-10-12 22:55:00 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,13 +2,11 @@ from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 
-# View to list all books and allow creating new books
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-# View to retrieve, update, or delete a single book
 class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
