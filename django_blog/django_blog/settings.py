577947a0e5e6e0e1c04e161500f573d8d7166f22
Zanelisiwe <ntombelazanelisiwe@outlook.com>
2025-10-23 14:45:25 +0200

    update2

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -82,6 +82,7 @@ DATABASES = {
         'PASSWORD': '',  
         'HOST': '',      
         'PORT': '',    
+    }
 }
 
 
--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import register_view, profile_view
+from blog.views import register_view, profile_view
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
