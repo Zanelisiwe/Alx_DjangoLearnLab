2df961e9a8589232deed94c0019beffc3a9ca598
Zanelisiwe <ntombelazanelisiwe@outlook.com>
2025-10-25 23:42:57 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     # home page (works with LOGIN_REDIRECT_URL = "/")
     path('',TemplateView.as_view(template_name='blog/base.html'), name='home'),
     path('', views.PostListView.as_view(), name='post_list'),
-    path('posts/', views.PostListView.as_view(), name='post_list_alt'),  
-    path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('post/', views.PostListView.as_view(), name='post_list_alt'),  
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 
     # Auth routes
     path('login/', auth_views.LoginView.as_view(
